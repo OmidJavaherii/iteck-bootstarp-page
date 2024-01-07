5bb34b918beda671532011dcf9eefa2f53adc72f
Omid Javaheri <129387041+OmidJavaherii@users.noreply.github.com>
2024-01-07 12:58:08 +0330

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$('#owl-carousel1-3').owlCarousel({
+    autoplay:true,
+    rtl:true,
+    loop:true,
+    margin:20,
+    // nav:true,
+    center:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+
+$('#owl-carousel1-4').owlCarousel({
+    // autoplay:true,
+    rtl:true,
+    loop:true,
+    margin:5,
+    dots:true,
+    nav:true,
+    // center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#owl-carousel1-6').owlCarousel({
+    autoplay:true,
+    rtl:true,
+    loop:true,
+    margin:0,
+    dots:false,
+    nav:true,
+    // center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
